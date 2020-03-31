071f6de4a867570f8766dda3920c33bc5c66daf4
Caitlin Wang <caitlinwang97@gmail.com>
2020-03-30 21:09:27 -0400

    Add map

--- js/visualization.js
+++ js/visualization.js
@@ -3,4 +3,8 @@
   d3.csv("/data/country-programme-results-2019.csv", (data) => {
     console.log(data);
   });
+
+  var map = d3.geomap()
+    .geofile('lib/d3-geomap/topojson/world/countries.json')
+    .draw(d3.select('#map'));
 })());
